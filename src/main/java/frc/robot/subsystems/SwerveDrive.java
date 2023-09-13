d114fcd5ecaaf0ee9e6cda761b397bc596cf7eaf
Sheraz Fayyaz <Sheraz24132@gmail.com>
2023-09-13 10:15:19 -0700

    Constants, SwerveDrive, SwerveModule subsystems
    Need to add robotcontainer bindings, and swervedrive/module commands

---
+++ src/main/java/frc/robot/subsystems/SwerveDrive.java
@@ -0,0 +1,388 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.can.TalonFX;
+import com.ctre.phoenix.sensors.CANCoder;
+import com.ctre.phoenix.sensors.Pigeon2;
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.estimator.SwerveDrivePoseEstimator;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Transform2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
+import edu.wpi.first.math.kinematics.SwerveModulePosition;
+import edu.wpi.first.math.kinematics.SwerveModuleState;
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.networktables.DoublePublisher;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.RobotBase;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.smartdashboard.MechanismLigament2d;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.CAN;
+import frc.robot.Constants.SWERVE_DRIVE;
+import frc.robot.Constants.SWERVE_DRIVE.SWERVE_MODULE_POSITION;
+import frc.robot.utils.ModuleMap;
+import java.util.HashMap;
+import java.util.Map;
+
+public class SwerveDrive extends SubsystemBase implements AutoCloseable {
+
+  private final HashMap<SWERVE_MODULE_POSITION, SwerveModule> m_swerveModules =
+      new HashMap<>(
+          Map.of(
+              SWERVE_MODULE_POSITION.FRONT_LEFT,
+                  new SwerveModule(
+                      SWERVE_MODULE_POSITION.FRONT_LEFT,
+                      new TalonFX(CAN.frontLeftTurnMotor),
+                      new TalonFX(CAN.frontLeftDriveMotor),
+                      new CANCoder(CAN.frontLeftCanCoder),
+                      SWERVE_DRIVE.frontLeftCANCoderOffset),
+              SWERVE_MODULE_POSITION.FRONT_RIGHT,
+                  new SwerveModule(
+                      SWERVE_MODULE_POSITION.FRONT_RIGHT,
+                      new TalonFX(CAN.frontRightTurnMotor),
+                      new TalonFX(CAN.frontRightDriveMotor),
+                      new CANCoder(CAN.frontRightCanCoder),
+                      SWERVE_DRIVE.frontRightCANCoderOffset),
+              SWERVE_MODULE_POSITION.BACK_LEFT,
+                  new SwerveModule(
+                      SWERVE_MODULE_POSITION.BACK_LEFT,
+                      new TalonFX(CAN.backLeftTurnMotor),
+                      new TalonFX(CAN.backLeftDriveMotor),
+                      new CANCoder(CAN.backLeftCanCoder),
+                      SWERVE_DRIVE.backLeftCANCoderOffset),
+              SWERVE_MODULE_POSITION.BACK_RIGHT,
+                  new SwerveModule(
+                      SWERVE_MODULE_POSITION.BACK_RIGHT,
+                      new TalonFX(CAN.backRightTurnMotor),
+                      new TalonFX(CAN.backRightDriveMotor),
+                      new CANCoder(CAN.backRightCanCoder),
+                      SWERVE_DRIVE.backRightCANCoderOffset)));
+
+  private final Pigeon2 m_pigeon = new Pigeon2(CAN.pigeon, "rio");
+  private double m_rollOffset;
+
+  private boolean m_limitJoystickInput = false;
+
+  private final SwerveDrivePoseEstimator m_odometry;
+
+  private MechanismLigament2d m_swerveChassis2d;
+
+  @SuppressWarnings("CanBeFinal")
+
+  private DoublePublisher pitchPub, rollPub, yawPub, odometryXPub, odometryYPub, odometryYawPub;
+
+  private boolean useHeadingTarget = false;
+  private double m_desiredHeadingRadians;
+
+  private final PIDController m_xController =
+      new PIDController(SWERVE_DRIVE.kP_X, SWERVE_DRIVE.kI_X, SWERVE_DRIVE.kD_X);
+  private final PIDController m_yController =
+      new PIDController(SWERVE_DRIVE.kP_Y, SWERVE_DRIVE.kI_Y, SWERVE_DRIVE.kD_Y);
+  private final PIDController m_turnController =
+      new PIDController(SWERVE_DRIVE.kP_Theta, SWERVE_DRIVE.kI_Theta, SWERVE_DRIVE.kD_Theta);
+
+  private double m_rotationOutput;
+
+  ChassisSpeeds chassisSpeeds;
+  private final double m_maxVelocity = SWERVE_DRIVE.kMaxSpeedMetersPerSecond;
+  private final double m_limitedVelocity = SWERVE_DRIVE.kLimitedSpeedMetersPerSecond;
+  private double m_currentMaxVelocity = m_maxVelocity;
+
+  public SwerveDrive() {
+    m_pigeon.configFactoryDefault();
+    m_pigeon.setYaw(0);
+    m_odometry =
+        new SwerveDrivePoseEstimator(
+            SWERVE_DRIVE.kSwerveKinematics,
+            getHeadingRotation2d(),
+            getSwerveDriveModulePositionsArray(),
+            new Pose2d());
+
+    m_turnController.enableContinuousInput(-Math.PI, Math.PI);
+
+    if (RobotBase.isReal()) {
+      Timer.delay(1);
+      resetModulesToAbsolute();
+    }
+
+    initSmartDashboard();
+  }
+
+  private void resetModulesToAbsolute() {
+    for (SwerveModule module : ModuleMap.orderedValuesList(m_swerveModules))
+      module.resetAngleToAbsolute();
+  }
+
+  public void setJoystickLimit(boolean limit) {
+    m_limitJoystickInput = limit;
+  }
+
+  public void drive(
+      double throttle,
+      double strafe,
+      double rotation,
+      boolean isFieldRelative,
+      boolean isOpenLoop) {
+    if (m_limitJoystickInput) {
+      throttle *= m_limitedVelocity;
+      strafe *= m_limitedVelocity;
+      rotation *= SWERVE_DRIVE.kLimitedRotationRadiansPerSecond;
+    } else {
+      throttle *= m_currentMaxVelocity;
+      strafe *= m_currentMaxVelocity;
+      rotation *= SWERVE_DRIVE.kMaxRotationRadiansPerSecond;
+    }
+
+    /** Setting field vs Robot Relative */
+    if (useHeadingTarget) {
+      rotation = m_rotationOutput;
+      chassisSpeeds =
+          ChassisSpeeds.fromFieldRelativeSpeeds(throttle, strafe, rotation, getHeadingRotation2d());
+    } else {
+      chassisSpeeds =
+          isFieldRelative
+              ? ChassisSpeeds.fromFieldRelativeSpeeds(
+                  throttle, strafe, rotation, getHeadingRotation2d())
+              : new ChassisSpeeds(throttle, strafe, rotation);
+    }
+
+    Map<SWERVE_MODULE_POSITION, SwerveModuleState> moduleStates =
+        ModuleMap.of(SWERVE_DRIVE.kSwerveKinematics.toSwerveModuleStates(chassisSpeeds));
+
+    SwerveDriveKinematics.desaturateWheelSpeeds(
+        ModuleMap.orderedValues(moduleStates, new SwerveModuleState[0]), m_currentMaxVelocity);
+
+    for (SwerveModule module : ModuleMap.orderedValuesList(m_swerveModules))
+      module.setDesiredState(moduleStates.get(module.getModulePosition()), isOpenLoop);
+  }
+
+  /** Set robot heading to a clear target */
+  public void setRobotHeadingRadians(double radians) {
+    m_desiredHeadingRadians = MathUtil.inputModulus(radians, -Math.PI, Math.PI);
+  }
+
+  public void calculateRotationSpeed() {
+    if (Math.abs(getHeadingRotation2d().getRadians() - m_desiredHeadingRadians)
+        > Units.degreesToRadians(1))
+      m_rotationOutput =
+          m_turnController.calculate(getHeadingRotation2d().getRadians(), m_desiredHeadingRadians);
+    else m_rotationOutput = 0;
+  }
+
+  /*
+   * ability to let head of swerve drive face the target
+   */
+  public void enableHeadingTarget(boolean enable) {
+    useHeadingTarget = enable;
+  }
+
+  public void setSwerveModuleStates(SwerveModuleState[] states, boolean isOpenLoop) {
+    SwerveDriveKinematics.desaturateWheelSpeeds(states, m_currentMaxVelocity);
+
+    for (SwerveModule module : ModuleMap.orderedValuesList(m_swerveModules))
+      module.setDesiredState(states[module.getModulePosition().ordinal()], isOpenLoop);
+  }
+
+  public void setSwerveModuleStatesAuto(SwerveModuleState[] states) {
+    setSwerveModuleStates(states, false);
+  }
+
+  public void setChassisSpeed(ChassisSpeeds chassisSpeeds) {
+    var states = SWERVE_DRIVE.kSwerveKinematics.toSwerveModuleStates(chassisSpeeds);
+    setSwerveModuleStates(states, false);
+  }
+
+  public void setOdometry(Pose2d pose) {
+    if (RobotBase.isSimulation()) {
+      m_pigeon.getSimCollection().setRawHeading(pose.getRotation().getDegrees());
+    } else m_pigeon.setYaw(pose.getRotation().getDegrees());
+    m_odometry.resetPosition(getHeadingRotation2d(), getSwerveDriveModulePositionsArray(), pose);
+
+    for (var position : SWERVE_MODULE_POSITION.values()) {
+      var transform =
+          new Transform2d(
+              SWERVE_DRIVE.kModuleTranslations.get(position), Rotation2d.fromDegrees(0));
+      var modulePose = pose.plus(transform);
+      getSwerveModule(position).resetAngle(pose.getRotation().getDegrees());
+      getSwerveModule(position).setModulePose(modulePose);
+    }
+  }
+
+  public void setRollOffset() {
+    m_rollOffset = -m_pigeon.getRoll(); // -2.63
+  }
+
+  public double getRollOffsetDegrees() {
+    return m_rollOffset;
+  }
+
+  public double getPitchDegrees() {
+    return m_pigeon.getPitch();
+  }
+
+  public double getRollDegrees() {
+   return m_pigeon.getRoll();
+  }
+
+  public double getHeadingDegrees() {
+    return m_pigeon.getYaw();
+  }
+
+  public Rotation2d getHeadingRotation2d() {
+    return Rotation2d.fromDegrees(getHeadingDegrees());
+  }
+
+  public Pose2d getPoseMeters() {
+    return m_odometry.getEstimatedPosition();
+  }
+
+  public SwerveModule getSwerveModule(SWERVE_MODULE_POSITION modulePosition) {
+    return m_swerveModules.get(modulePosition);
+  }
+
+  public Map<SWERVE_MODULE_POSITION, SwerveModuleState> getModuleStates() {
+    Map<SWERVE_MODULE_POSITION, SwerveModuleState> map = new HashMap<>();
+    for (SWERVE_MODULE_POSITION i : m_swerveModules.keySet())
+      map.put(i, m_swerveModules.get(i).getState());
+    return map;
+  }
+
+  public Map<SWERVE_MODULE_POSITION, SwerveModulePosition> getModulePositions() {
+    Map<SWERVE_MODULE_POSITION, SwerveModulePosition> map = new HashMap<>();
+    for (SWERVE_MODULE_POSITION i : m_swerveModules.keySet())
+      map.put(i, m_swerveModules.get(i).getPosition());
+    return map;
+  }
+
+  public SwerveModulePosition[] getSwerveDriveModulePositionsArray() {
+    return ModuleMap.orderedValues(getModulePositions(), new SwerveModulePosition[0]);
+  }
+
+  public Map<SWERVE_MODULE_POSITION, Pose2d> getModulePoses() {
+    Map<SWERVE_MODULE_POSITION, Pose2d> map = new HashMap<>();
+    for (SWERVE_MODULE_POSITION i : m_swerveModules.keySet())
+      map.put(i, m_swerveModules.get(i).getModulePose());
+    return map;
+  }
+
+  public boolean getModuleInitStatus() {
+    for (SWERVE_MODULE_POSITION i : m_swerveModules.keySet()) {
+      if (!m_swerveModules.get(i).getInitSuccess()) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  public MechanismLigament2d getLigament() {
+    return m_swerveChassis2d;
+  }
+
+  public void setLigament(MechanismLigament2d ligament) {
+    m_swerveChassis2d = ligament;
+  }
+
+  public PIDController getXPidController() {
+    return m_xController;
+  }
+
+  public PIDController getYPidController() {
+    return m_yController;
+  }
+
+  public PIDController getThetaPidController() {
+    return m_turnController;
+  }
+
+  public void setNeutralMode(NeutralMode mode) {
+    for (SwerveModule module : m_swerveModules.values()) {
+      //      module.setDriveNeutralMode(mode);
+      module.setTurnNeutralMode(mode);
+    }
+  }
+
+  public void setMaxVelocity(double mps) {
+    m_currentMaxVelocity = mps;
+  }
+
+  public SwerveDrivePoseEstimator getOdometry() {
+    return m_odometry;
+  }
+
+  public void resetGyro() {
+    //    if (DriverStation.isFMSAttached() && Controls.getAllianceColor() ==
+    // DriverStation.Alliance.Red)
+    //      m_pigeon.setYaw(180);
+    //    else
+    m_pigeon.setYaw(0);
+    m_pigeon.setAccumZAngle(0);
+  }
+
+  public void updateOdometry() {
+    m_odometry.update(getHeadingRotation2d(), getSwerveDriveModulePositionsArray());
+
+    for (SwerveModule module : ModuleMap.orderedValuesList(m_swerveModules)) {
+      Transform2d moduleTransform =
+          new Transform2d(
+              SWERVE_DRIVE.kModuleTranslations.get(module.getModulePosition()),
+              module.getHeadingRotation2d());
+      module.setModulePose(getPoseMeters().transformBy(moduleTransform));
+    }
+  }
+
+  private void initSmartDashboard() {
+    SmartDashboard.putData(this);
+
+    var swerveTab =
+        NetworkTableInstance.getDefault().getTable("Shuffleboard").getSubTable("Swerve");
+    pitchPub = swerveTab.getDoubleTopic("Pitch").publish();
+    rollPub = swerveTab.getDoubleTopic("Roll").publish();
+    yawPub = swerveTab.getDoubleTopic("Yaw").publish();
+    odometryXPub = swerveTab.getDoubleTopic("Odometry X").publish();
+    odometryYPub = swerveTab.getDoubleTopic("Odometry Y").publish();
+    odometryYawPub = swerveTab.getDoubleTopic("Odometry Yaw").publish();
+  }
+
+  private void updateSmartDashboard() {
+    SmartDashboard.putNumber("gyro " + m_pigeon + " heading", getHeadingDegrees());
+    SmartDashboard.putBoolean("Swerve Module Init Status", getModuleInitStatus());
+    SmartDashboard.putNumber("Roll Offset", m_rollOffset);
+
+    pitchPub.set(getPitchDegrees());
+    rollPub.set(getRollDegrees() + getRollOffsetDegrees());
+    yawPub.set(getHeadingDegrees());
+      odometryXPub.set(getOdometry().getEstimatedPosition().getX());
+      odometryYPub.set(getOdometry().getEstimatedPosition().getY());
+      odometryYawPub.set(getOdometry().getEstimatedPosition().getRotation().getDegrees());
+    }
+
+  @Override
+  public void periodic() {
+    if (DriverStation.isEnabled() && useHeadingTarget) {
+      calculateRotationSpeed();
+    }
+
+    updateOdometry();
+    updateSmartDashboard();
+  }
+
+  @Override
+  public void simulationPeriodic() {
+  }
+
+  @Override
+  public void close() throws Exception {
+    if (m_swerveChassis2d != null) m_swerveChassis2d.close();
+    for (var module : ModuleMap.orderedValuesList(m_swerveModules)) module.close();
+  }
+}
---
+++ src/main/java/frc/robot/subsystems/SwerveModule.java
@@ -0,0 +1,240 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.DemandType;
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.can.TalonFX;
+import com.ctre.phoenix.sensors.CANCoder;
+import com.ctre.phoenix.sensors.CANCoderStatusFrame;
+import edu.wpi.first.math.controller.SimpleMotorFeedforward;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.kinematics.SwerveModulePosition;
+import edu.wpi.first.math.kinematics.SwerveModuleState;
+import edu.wpi.first.networktables.BooleanPublisher;
+import edu.wpi.first.networktables.DoublePublisher;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.util.datalog.DataLog;
+import edu.wpi.first.util.datalog.DoubleLogEntry;
+import edu.wpi.first.wpilibj.DataLogManager;
+import edu.wpi.first.wpilibj.RobotBase;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.SWERVE_DRIVE;
+import frc.robot.Constants.SWERVE_DRIVE.SWERVE_MODULE_POSITION;
+import frc.robot.Constants.SWERVE_MODULE;
+import frc.robot.utils.CtreUtils;
+
+public class SwerveModule extends SubsystemBase implements AutoCloseable {
+  private final SWERVE_MODULE_POSITION m_modulePosition;
+  private final int m_moduleNumber;
+  private final TalonFX m_turnMotor;
+  private final TalonFX m_driveMotor;
+  private final CANCoder m_angleEncoder;
+  private final double m_angleOffset;
+  private double m_lastAngle;
+  private Pose2d m_pose;
+  private boolean m_initSuccess = false;
+
+  private final SimpleMotorFeedforward feedforward =
+      new SimpleMotorFeedforward(
+          SWERVE_MODULE.ksDriveVoltSecondsPerMeter,
+          SWERVE_MODULE.kvDriveVoltSecondsSquaredPerMeter,
+          SWERVE_MODULE.kaDriveVoltSecondsSquaredPerMeter);
+  // Logging setup
+
+  private final DoubleLogEntry moduleTurnCurrentEntry;
+  private final DoubleLogEntry moduleDriveCurrentEntry;
+
+  private DoublePublisher moduleMotorHeadingPub, moduleEncoderHeadingPub;
+  private BooleanPublisher moduleEncoderHealthPub;
+
+  public SwerveModule(
+      SWERVE_MODULE_POSITION modulePosition,
+      TalonFX turnMotor,
+      TalonFX driveMotor,
+      CANCoder angleEncoder,
+      double angleOffset) {
+    m_modulePosition = modulePosition;
+    m_moduleNumber = m_modulePosition.ordinal();
+    m_turnMotor = turnMotor;
+    m_driveMotor = driveMotor;
+    m_angleEncoder = angleEncoder;
+    m_angleOffset = angleOffset;
+
+    initModuleHeading();
+
+    m_turnMotor.configFactoryDefault();
+    m_turnMotor.configAllSettings(CtreUtils.generateTurnMotorConfig());
+    m_turnMotor.setInverted(true);
+    m_turnMotor.setSelectedSensorPosition(0);
+
+    m_driveMotor.configFactoryDefault();
+    m_driveMotor.configAllSettings(CtreUtils.generateDriveMotorConfig());
+    m_driveMotor.setInverted(false);
+    m_driveMotor.setNeutralMode(NeutralMode.Brake);
+    m_turnMotor.setNeutralMode(NeutralMode.Brake);
+
+    // m_angleEncoder.configMagnetOffset(m_angleOffset);
+    m_lastAngle = getHeadingDegrees();
+
+    initSmartDashboard();
+    DataLog m_log = DataLogManager.getLog();
+    moduleTurnCurrentEntry =
+        new DoubleLogEntry(m_log, "/swerve/" + m_modulePosition.name() + "/turnCurrent");
+    moduleDriveCurrentEntry =
+        new DoubleLogEntry(m_log, "/swerve/" + m_modulePosition.name() + "/driveCurrent");
+
+    // To distinguish modules in CommandScheduler
+    setName("SwerveModule_" + m_modulePosition.ordinal());
+  }
+
+  private void initModuleHeading() {
+    if (RobotBase.isReal()) Timer.delay(0.2);
+    m_angleEncoder.configFactoryDefault();
+    m_angleEncoder.configAllSettings(CtreUtils.generateCanCoderConfig());
+    m_angleEncoder.setStatusFramePeriod(CANCoderStatusFrame.SensorData, 255);
+    m_angleEncoder.setStatusFramePeriod(CANCoderStatusFrame.VbatAndFaults, 255);
+    resetAngleToAbsolute();
+
+    // Check if the offset was applied properly. Delay to give it some time to set
+    if (RobotBase.isReal()) {
+      Timer.delay(0.1);
+      m_initSuccess =
+          Math.abs(getHeadingDegrees() + m_angleOffset - m_angleEncoder.getAbsolutePosition())
+              < 1.0;
+    } else m_initSuccess = true;
+  }
+
+  public boolean getInitSuccess() {
+    return m_initSuccess;
+  }
+
+  public SWERVE_MODULE_POSITION getModulePosition() {
+    return m_modulePosition;
+  }
+
+  public void resetAngleToAbsolute() {
+    resetAngle(0);
+  }
+
+  public void resetAngle(double angle) {
+    double newAngle = m_angleEncoder.getAbsolutePosition() - m_angleOffset + angle;
+    m_turnMotor.setSelectedSensorPosition(newAngle / SWERVE_MODULE.kTurningMotorDistancePerPulse);
+  }
+
+  public double getHeadingDegrees() {
+    return m_turnMotor.getSelectedSensorPosition() * SWERVE_MODULE.kTurningMotorDistancePerPulse;
+  }
+
+  public Rotation2d getHeadingRotation2d() {
+    return Rotation2d.fromDegrees(getHeadingDegrees());
+  }
+
+  public double getVelocityMetersPerSecond() {
+    return m_driveMotor.getSelectedSensorVelocity()
+        * SWERVE_MODULE.kDriveMotorDistancePerPulse
+        * 10;
+  }
+
+  public double getDriveMeters() {
+    return m_driveMotor.getSelectedSensorPosition() * SWERVE_MODULE.kDriveMotorDistancePerPulse;
+  }
+
+  public void setDesiredState(SwerveModuleState desiredState, boolean isOpenLoop) {
+    desiredState = CtreUtils.optimize(desiredState, getHeadingRotation2d());
+
+    if (isOpenLoop) {
+      double percentOutput =
+          desiredState.speedMetersPerSecond / SWERVE_DRIVE.kMaxSpeedMetersPerSecond;
+      m_driveMotor.set(ControlMode.PercentOutput, percentOutput);
+    } else {
+      double velocity =
+          desiredState.speedMetersPerSecond / (SWERVE_MODULE.kDriveMotorDistancePerPulse * 10);
+      m_driveMotor.set(
+          ControlMode.Velocity,
+          velocity,
+          DemandType.ArbitraryFeedForward,
+          feedforward.calculate(desiredState.speedMetersPerSecond));
+    }
+
+    double angle =
+        (Math.abs(desiredState.speedMetersPerSecond)
+                <= (SWERVE_DRIVE.kMaxSpeedMetersPerSecond * 0.01))
+            ? m_lastAngle
+            : desiredState.angle
+                .getDegrees(); // Prevent rotating module if speed is less than 1%. Prevents
+    // Jittering.
+    m_turnMotor.set(ControlMode.Position, angle / SWERVE_MODULE.kTurningMotorDistancePerPulse);
+    m_lastAngle = angle;
+  }
+
+  public SwerveModuleState getState() {
+    return new SwerveModuleState(getVelocityMetersPerSecond(), getHeadingRotation2d());
+  }
+
+  public SwerveModulePosition getPosition() {
+    return new SwerveModulePosition(getDriveMeters(), getHeadingRotation2d());
+  }
+
+  public void setModulePose(Pose2d pose) {
+    m_pose = pose;
+  }
+
+  public Pose2d getModulePose() {
+    return m_pose;
+  }
+
+  public void setDriveNeutralMode(NeutralMode mode) {
+    m_driveMotor.setNeutralMode(mode);
+  }
+
+  public void setTurnNeutralMode(NeutralMode mode) {
+    m_turnMotor.setNeutralMode(mode);
+  }
+
+  private void initSmartDashboard() {
+    var moduleTab =
+        NetworkTableInstance.getDefault().getTable("Shuffleboard").getSubTable("Swerve");
+    moduleEncoderHeadingPub =
+        moduleTab.getDoubleTopic("Module (" + m_moduleNumber + ") Encoder Heading").publish();
+    moduleTab
+        .getDoubleTopic("Module (" + m_moduleNumber + ") Encoder Offset")
+        .publish()
+        .set(m_angleOffset);
+    moduleEncoderHealthPub =
+        moduleTab.getBooleanTopic("Module (" + m_moduleNumber + ") Encoder Health").publish();
+    moduleMotorHeadingPub =
+        moduleTab.getDoubleTopic("Module (" + m_moduleNumber + ") Motor Heading").publish();
+  }
+
+  private void updateSmartDashboard() {
+    moduleMotorHeadingPub.set(getHeadingDegrees());
+    moduleEncoderHealthPub.set(getInitSuccess());
+      moduleEncoderHeadingPub.set(m_angleEncoder.getAbsolutePosition());
+    }
+
+  public void updateLog() {
+    moduleTurnCurrentEntry.append(m_turnMotor.getMotorOutputVoltage());
+    moduleDriveCurrentEntry.append(m_driveMotor.getMotorOutputVoltage());
+  }
+
+  @Override
+  public void periodic() {
+    updateSmartDashboard();
+    //    updateLog();
+  }
+
+  @Override
+  public void simulationPeriodic() {
+   
+  }
+
+  @SuppressWarnings("RedundantThrows")
+  @Override
+  public void close() throws Exception {}
+}
